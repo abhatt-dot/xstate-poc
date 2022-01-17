c502d409a4b7599f0c572f050c8b49d5a9c7ad9b
Avni Bhatt <a.bhatt.2@elsevier.com>
2022-01-17 17:02:36 +0000

    not quite working state machine

--- src/App.js
+++ src/App.js
@@ -1,43 +1,47 @@
 import './App.css';
 import { useMachine } from "@xstate/react";
-import { createMachine } from 'xstate';
+import { assign, createMachine } from 'xstate';
 
 // we need:
 // show all the different states for searching: ready, loading and reset
 // state diagrams and user flows
 
 const searchMachine = createMachine({
-  id: 'search',
+  // id: 'search',
   initial: 'disabled',
   context: {
     search: null,
   },
   states: {
-    ready: {},
+    disabled: {},
     searching: {},
     reset: {}
   },
   on: {
     SEARCH: {
-      target: '.searching',
+      target: 'searching',
+      actions: assign({ search: (e) => e.search })
       // when you search change the button to green + persist a search term
     },
     RESET: {
       // when you click the button it shows reset button
       // and clears the search
     },
-
+    
   }
 })
 
 function App() {
   const [state, send] = useMachine(searchMachine);
-  const search = state.matches("active");
-  const { searchValue } = state.context;
+  // const search = state.matches("disabled");
+  const { search } = state.context;
+  console.log(search);
+  let inputValue;
+
   return (
     <div className="App">
-      <input type="text" id="name" name="name" value={searchValue} required size="20"></input>
-      <button onClick={() => send("SEARCH", { name: e.target.value })}> Search </button>
+      <input type="text" name="name"  onChange={(e) => inputValue = e.target.value}required size="20"></input>
+      <button id="search" onClick={(e) => send("SEARCH", { search: inputValue })}> {state.matches("disabled") ? "hello" : search} </button>
     </div>
   );
 }
